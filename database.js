ac23c2506890c467f57e46b8704cab51b79322db
Rosecore67 <cr67.rose@gmail.com>
2022-11-13 11:42:15 +0100

    ajout GET

--- database.js
+++ database.js
@@ -19,22 +19,59 @@ const database = mysql.createPool({
     console.error(err);
   });
 
-  database
-  .query("select * from movies")
-  .then(([movie]) => {
-    console.log(movie);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-  
-  database
-  .query("select * from users")
-  .then(([user]) => {
-    console.log(user);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+  const getMovies = (req, res) => {
+    let sql = "select * from movies";
+    const sqlValues = [];
 
+    if (req.query.color != null) {
+      sql += " where color = ?";
+      sqlValues.push(req.query.color);
+
+      if (req.query.max_duration != null) {
+        sql += " and duration <= ?";
+        sqlValues.push(req.query.max_duration);
+      }
+        } else if (req.query.max_duration != null) {
+          sql += " where duration <= ?";
+          sqlValues.push(req.query.max_duration);
+      } 
+  
+
+    database
+      .query(sql, sqlValues)
+      .then(([movies]) => {
+        res.json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };
+
+const getUsers = (req, res) => {
+      let sql = "select * from users";
+      const sqlValues = [];
+
+      if (req.query.language != null) {
+        sql += " where language = ?";
+      sqlValues.push(req.query.color);
+
+      if (req.query.city != null) {
+      sql += " and city = ?";
+      sqlValues.push(req.query.city);
+    }
+  } else if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+  }
+
+    database
+    .query("select * from users")
+    .then(([user]) => {
+      console.log(user);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
   module.exports = database;
